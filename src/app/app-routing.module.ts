da158ae942d288e274b2baccf90535ca56eb5d75
Guillaume Lerda <lerda.guillaume@gmail.com>
2017-11-29 19:42:34 +0100

    Connexion de plusieurs réseaux sociaux

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { CreateAccountComponent } from './authentication/create-account/create-account.component';
+import { LoginComponent } from './authentication/login/login.component';
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AuthGuard} from './auth-guard.service';
@@ -7,7 +9,11 @@ import { RecordsComponent } from './records/records.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'records', pathMatch: 'full'},
-  {path: 'authentication', component: AuthenticationComponent},
+  {path: 'authentication', component: AuthenticationComponent, children: [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'create-account', component: CreateAccountComponent},
+  ]},
   {path: 'records', component: RecordsComponent, canActivate: [AuthGuard]}
 ];
 
--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,23 +1,25 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
-import {AngularFireAuth} from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
-import {Observable} from 'rxjs/Observable';
-import {Router} from '@angular/router';
-import {LoadingService} from '../loading/loading.service';
-import {ToastService} from '../toast.service';
-import {Location} from '@angular/common';
-import {Subject} from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import { LoadingService } from '../loading/loading.service';
+import { ToastService } from '../toast.service';
+import { Location } from '@angular/common';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class AuthService {
   userDetails: firebase.User = null;
   connectionChanged = new Subject<firebase.User>();
 
+  tempProviderCredentials: firebase.auth.AuthCredential = null;
+
 
   constructor(private firebaseAuth: AngularFireAuth, private router: Router,
-              private loadingService: LoadingService, private toastService: ToastService) {
+    private loadingService: LoadingService, private toastService: ToastService) {
 
     firebaseAuth.authState.subscribe(
       (user) => {
@@ -32,6 +34,13 @@ export class AuthService {
               this.toastService.toast('Connecté en tant que ' + user.email);
             }
             this.router.navigate(['/records']);
+
+            if (this.tempProviderCredentials != null) {
+              this.userDetails.linkWithCredential(this.tempProviderCredentials).then((result) => {
+                this.toastService.toast('Comptes liés avec succés');
+              });
+              this.tempProviderCredentials = null;
+            }
           } else {
             this.logout();
             this.toastService.toast('Merci de valider votre adresse mail');
@@ -54,8 +63,10 @@ export class AuthService {
           this.toastService.toast('Compte ' + email + ' créé !');
         })
         .catch(err => {
-          this.toastService.toast(err);
           this.loadingService.isLoading = false;
+          if (err.code === 'auth/email-already-in-use') {
+            this.toastService.toast('Email déjà utilisé');
+          }
         });
     } else {
       this.toastService.toast('Les deux mots de passe renseignés sont différents');
@@ -64,30 +75,56 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
-    this.firebaseAuth
-      .auth
-      .signInWithEmailAndPassword(email, password)
+    this.firebaseAuth.auth.signInWithEmailAndPassword(email, password)
       .then(value => {
       }).catch(err => {
-      this.loadingService.isLoading = false;
-    });
+        this.loadingService.isLoading = false;
+      });
   }
 
   signInWithGoogle() {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
     this.firebaseAuth.auth.signInWithPopup(
-      new firebase.auth.GoogleAuthProvider()
+      googleProvider
     ).then(user => {
     }).catch(err => {
       this.loadingService.isLoading = false;
+      if (err.code === 'auth/account-exists-with-different-credential') {
+        this.prepareForSocialLinking(err);
+      }
     });
   }
 
   signInWithFacebook() {
+    const facebookProvider = new firebase.auth.FacebookAuthProvider();
     this.firebaseAuth.auth.signInWithPopup(
-      new firebase.auth.FacebookAuthProvider()
+      facebookProvider
     ).then(user => {
     }).catch(err => {
       this.loadingService.isLoading = false;
+      if (err.code === 'auth/account-exists-with-different-credential') {
+        this.prepareForSocialLinking(err);
+      }
+    });
+  }
+
+  prepareForSocialLinking(errorData) {
+    this.firebaseAuth.auth.fetchProvidersForEmail(errorData.email).then((result) => {
+      if (result[0] === 'password') {
+        this.tempProviderCredentials = errorData.credential;
+        this.toastService.toast('Vous possedez déjà un compte. Connectez vous avec' +
+        ' Email/Password pour lier votre compte ' + errorData.credential.providerId);
+      }
+      if (result[0] === 'google.com') {
+        this.tempProviderCredentials = errorData.credential;
+        this.toastService.toast('Vous possedez déjà un compte. Connectez vous avec' +
+         ' Google pour lier votre compte ' + errorData.credential.providerId);
+      }
+      if (result[0] === 'facebook.com') {
+        this.tempProviderCredentials = errorData.credential;
+        this.toastService.toast('Vous possedez déjà un compte. Connectez vous avec' +
+         ' Facebook pour lier votre compte ' + errorData.credential.providerId);
+      }
     });
   }
 
