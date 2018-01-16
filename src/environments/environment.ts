514bb5d8646ca70547f30858c186a40a1f25dc96
Guillaume Lerda <lerda.guillaume@gmail.com>
2018-01-16 13:51:43 +0100

    guillemets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBVwqINlEuptqJ_ChBhCaP0Xuiqedt1AjA",
-    authDomain: "reckit-4c2d3.firebaseapp.com",
-    databaseURL: "https://reckit-4c2d3.firebaseio.com",
-    projectId: "reckit-4c2d3",
-    storageBucket: "reckit-4c2d3.appspot.com",
-    messagingSenderId: "718078757460"
+    apiKey: 'AIzaSyBVwqINlEuptqJ_ChBhCaP0Xuiqedt1AjA',
+    authDomain: 'reckit-4c2d3.firebaseapp.com',
+    databaseURL: 'https://reckit-4c2d3.firebaseio.com',
+    projectId: 'reckit-4c2d3',
+    storageBucket: 'reckit-4c2d3.appspot.com',
+    messagingSenderId: '718078757460'
   }
 };
