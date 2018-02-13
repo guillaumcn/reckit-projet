b97846a7e4bbc57817c25888801d32444b78cf8e
Guillaume Lerda <lerda.guillaume@gmail.com>
2018-02-13 14:28:13 +0100

    copie des réponses au reload

--- src/app/record-detail/record-comment/record-comment.component.ts
+++ src/app/record-detail/record-comment/record-comment.component.ts
@@ -1,45 +1,91 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {Record} from '../../record.model';
 import {Comment} from '../../comment.model';
 import {RecordService} from '../../record.service';
 import {AuthService} from '../../authentication/auth.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-record-comment',
   templateUrl: './record-comment.component.html',
   styleUrls: ['./record-comment.component.css']
 })
-export class RecordCommentComponent implements OnInit {
+export class RecordCommentComponent implements OnInit, OnDestroy {
 
   askaquestion = '';
   @Input('selectedRecord') selectedRecord: Record = new Record();
 
-  comments: Comment[];
+  comments: Comment[] = [];
+  nbComments: number;
 
   @Output() spaceActive = new EventEmitter<boolean>();
+
+  // We will add subscriptions to observable here and unsubscribe when destroying the component
+  subscriptions: Subscription[] = [];
 
   constructor(private recordService: RecordService, private authService: AuthService) {
   }
 
   ngOnInit() {
-    this.recordService.commentsList(this.selectedRecord.key, 5).subscribe((comments) => {
-      this.comments = comments.reverse();
-    });
+    this.nbComments = 0;
+    this.moreComments();
   }
 
-  getAnswers(index: string) {
+  moreComments() {
+    // Unsubscribe all observables
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
+
+    this.nbComments = this.nbComments + 5;
+
+    this.subscriptions.push(this.recordService.commentsList(this.selectedRecord.key, this.nbComments).subscribe((comments) => {
+      const moveAnswers = {};
+      for (let i = 0; i < this.comments.length; i++) {
+        if (this.comments[i]['answers']) {
+          moveAnswers[this.comments[i].key] = {};
+          moveAnswers[this.comments[i].key]['answers'] = this.comments[i]['answers'];
+          moveAnswers[this.comments[i].key]['tempAnswer'] = this.comments[i]['tempAnswer'];
+          moveAnswers[this.comments[i].key]['answerLimit'] = this.comments[i]['answerLimit'];
+          moveAnswers[this.comments[i].key]['subscription'] = this.comments[i]['subscription'];
+        }
+      }
+
+      this.comments = comments.reverse();
+
+      for (let i = 0; i < this.comments.length; i++) {
+        if (moveAnswers[this.comments[i].key]) {
+          this.comments[i]['answers'] = moveAnswers[this.comments[i].key]['answers'];
+          this.comments[i]['tempAnswer'] = moveAnswers[this.comments[i].key]['tempAnswer'];
+          this.comments[i]['answerLimit'] = moveAnswers[this.comments[i].key]['answerLimit'];
+          this.comments[i]['subscription'] = moveAnswers[this.comments[i].key]['subscription'];
+        }
+      }
+    }));
+  }
+
+  getAnswers(index: number) {
     if (!this.comments[index]['answers']) {
       this.comments[index]['answers'] = [];
       this.comments[index]['tempAnswer'] = '';
-      this.comments[index]['subscription'] = this.recordService.answersList(this.selectedRecord.key, this.comments[index].key, 5).subscribe((answers) => {
-        this.comments[index]['answers'] = answers.reverse();
-      });
+      this.comments[index]['answerLimit'] = 0;
+      this.moreAnswers(index);
     } else {
       delete this.comments[index]['answers'];
       delete this.comments[index]['tempAnswer'];
       this.comments[index]['subscription'].unsubscribe();
       delete this.comments[index]['subscription'];
     }
+  }
+
+  moreAnswers(index: number) {
+    this.comments[index]['answerLimit'] += 5;
+    if (this.comments[index]['subscription']) {
+      this.comments[index]['subscription'].unsubscribe();
+    }
+    this.comments[index]['subscription'] = this.recordService.answersList(this.selectedRecord.key, this.comments[index].key, this.comments[index]['answerLimit']).subscribe((answers: { answerer: string, date: number, textAnswer: string }[]) => {
+      this.comments[index]['answers'] = answers.reverse();
+    });
   }
 
   addQuestion() {
@@ -62,5 +108,12 @@ export class RecordCommentComponent implements OnInit {
 
   timestampToLocaleString(timestamp) {
     return new Date(timestamp).toLocaleString();
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe all observables
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
   }
 }
